6f1c196f174488c1f3184f943454ea3e0102f52e
Emmaline <emmalinerose19@gmail.com>
2023-02-24 21:18:23 -0600

    accordian list displays search queries

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,33 +1,23 @@
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const RepoList = (props) => {
-
-    const {name,
-          language
-          // description,
-          // stargazers_count,
-          // forks_count,
-          // created_at,
-          // commitTitle,
-          // committerUsername,
-          // commitHash,
-          // dateCommitCreated
-        } = props
-
-    const listLanguage = () => {
-      return language.map((title) => {
-        return (
-          <Typography>{title.name}</Typography>
-        )
-      }
-        
-      )
-    }
+  const {
+    name,
+    language,
+    description,
+    stargazers_count,
+    forks_count,
+    created_at,
+    // commitTitle,
+    // committerUsername,
+    // commitHash,
+    // dateCommitCreated
+  } = props;
 
   // const repoName = props.parseData.name
   // const repoLanguage = props.parseData.language
@@ -35,9 +25,8 @@ const RepoList = (props) => {
   // const repoStars = props.parseData.stargazers_count
   // const repoForks = props.parseData.forks_count
   // const repoCreationDate = props.parseData.created_at
-  
 
-  return(
+  return (
     <div>
       <Accordion>
         <AccordionSummary
@@ -48,94 +37,23 @@ const RepoList = (props) => {
           <Typography>{name}</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            {listLanguage()}
-            {/* {repoDescription}
-            {repoStars}
-            {repoForks}
-            {repoCreationDate} */}
-          </Typography>
+          <Typography>{language}</Typography>
+          <Typography>{description}</Typography>
+          <Typography>{stargazers_count}</Typography>
+          <Typography>{forks_count}</Typography>
+          <Typography>{created_at}</Typography>
         </AccordionDetails>
-      </Accordion>
-      <Accordion>
+        {/* </Accordion>
+      <Accordion> */}
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+        ></AccordionSummary>
+        <AccordionDetails></AccordionDetails>
       </Accordion>
     </div>
-  )
-}
+  );
+};
 
-export default RepoList
-
-// const RepoList = (props) => {
-
-//   // const { name,
-//   //         language,
-//   //         description,
-//   //         starCount,
-//   //         forkCount,
-//   //         dateRepoCreated,
-//   //         commitTitle,
-//   //         committerUsername,
-//   //         commitHash,
-//   //         dateCommitCreated
-//   //       } = props
-  
-//   // const repoLanguages = () => {
-//   //   return language.map((language) => {
-//   //     return (
-//   //       <ul>{language.name}</ul>
-//   //     )
-//   //   })
-//   // }
-
-//   const {name} = props
-
-//   const repoName = props.parseData.name
-//   // const repoLanguage = props.parsedData.language
-//   // const repoDescription = props.parsedData.description
-//   // const repoStars = props.parsedData.stargazers_count
-//   // const repoForks = props.parsedData.forks_count
-//   // const repoCreationDate = props.parsedData.created_at
-  
-
-//   return (
-//     <>
-//       <div>{repoName}</div>
-//       {/* <div class="accordion" id="accordionPanelsStayOpenExample">
-//           <div class="accordion-item">
-//                   <h2 
-//                       // key={data.id}
-//                       value={repoName}
-//                       class="accordion-header" id="panelsStayOpen-headingOne">
-//                   <button 
-//                       class="accordion-button" 
-//                       type="button" 
-//                       data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-//                   {data.repo}
-//               </button>
-//               </h2>            
-//           <div 
-//               id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-//           <div 
-//               class="accordion-body">
-//           <strong>{language}</strong>
-//           </div>
-//           </div>
-//           </div>
-//       </div> */}
-//     </>
-//   )
-// }
-// export default RepoList
+export default RepoList;
